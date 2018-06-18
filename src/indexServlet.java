8830259bf92ef0256a26f21e26c9e4f347dcbd2a
mansoorsheikh <mansoorsheikh9@gmail.com>
2018-06-18 13:53:39 +0500

    Initialization

---
+++ src/indexServlet.java
@@ -0,0 +1,42 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+/**
+ * Servlet implementation class indexServlet
+ */
+@WebServlet("/indexServlet")
+public class indexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public indexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String objectToReturn = "{ key1: 'value1', key2: 'value2' }";
+		response.setHeader("contentType", "application/json");
+		response.getWriter().append(objectToReturn);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
